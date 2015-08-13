7bc3bbdd069b38e211b69b66e709ee696afcf599
AdrianFall <adrianq92@hotmail.com>
2015-08-13 17:22:13 +0100

    log4j added

--- src/main/java/core/controller/IndexController.java
+++ src/main/java/core/controller/IndexController.java
@@ -1,5 +1,7 @@
 package core.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +16,13 @@ public class IndexController {
     @RequestMapping(method = RequestMethod.GET)
 
     public String index() {
+        final Log logger = LogFactory.getLog(getClass());
+        if (logger != null) {
+            System.out.println("Logger exists -> class = " + logger.getClass().toString());
+
+            System.out.println("Debug is " + ((logger.isDebugEnabled()) ? "enabled" : "disabled" ));
+        } else
+            System.out.println("Logger does not exist");
         return "index";
     }
 }
